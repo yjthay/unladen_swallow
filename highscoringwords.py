c99a8851cebe2254cf4b638ce2c39dce7f316d39
yjthay <thayyeejie@gmail.com>
2023-09-11 11:50:50 +0100

    Include type checking as well as including docstring comments

--- highscoringwords.py
+++ highscoringwords.py
@@ -1,20 +1,20 @@
 __author__ = 'codesse'
 
 from collections import Counter
+from typing import List, Dict, Tuple
 
 
 class HighScoringWords:
-    MAX_LEADERBOARD_LENGTH = 100  # the maximum number of items that can appear in the leaderboard
-    MIN_WORD_LENGTH = 3  # words must be at least this many characters long
-    letter_values = {}
-    valid_words = []
+    MAX_LEADERBOARD_LENGTH: int = 100  # the maximum number of items that can appear in the leaderboard
+    MIN_WORD_LENGTH: int = 3  # words must be at least this many characters long
+    letter_values: Dict = {}
+    valid_words: List = []
 
-    def __init__(self, validwords='wordlist.txt', lettervalues='letterValues.txt'):
+    def __init__(self, validwords: str = 'wordlist.txt', lettervalues: str = 'letterValues.txt'):
         """
         Initialise the class with complete set of valid words and letter values by parsing text files containing the data
         :param validwords: a text file containing the complete set of valid words, one word per line
         :param lettervalues: a text file containing the score for each letter in the format letter:score one per line
-        :return:
         """
         with open(validwords) as f:
             self.valid_words = f.read().splitlines()
@@ -25,7 +25,7 @@ class HighScoringWords:
                 (key, val) = line.split(':')
                 self.letter_values[str(key).strip().lower()] = int(val)
 
-    def score(self, word):
+    def score(self, word: str) -> int:
         """
         Scoring function of the word and returns the score of the word in the Scrabble Game
         :param word: string that needs to be validated
@@ -37,12 +37,15 @@ class HighScoringWords:
         res = sum([self.letter_values[char.lower()] for char in word])
         return res
 
-    def is_subset(self, superset, subset):
+    @staticmethod
+    def is_subset(superset: Dict[str, int], subset: Dict[str, int]) -> bool:
         """
-        Return
-        :param superset: Dictionary
-        :param subset: Dictionary
-        :return: boolean where True when subset is in superset and False when subset is not in superset
+        Boolean function set to True if subset dictionary is a subset of superset on the conditions of:
+        1) All keys in subset are in superset
+        2) All values of keys in subset are less than or equal to the corresponding key-values in superset
+        :param superset: Dictionary with alphabet as the key and the number of occurrences of that alphabet as value
+        :param subset: Dictionary with alphabet as the key and the number of occurrences of that alphabet as value
+        :return: Boolean where True when subset is in superset and False when subset is not in superset
         """
         if not set(superset.keys()).issuperset(set(subset.keys())):
             return False
@@ -51,12 +54,20 @@ class HighScoringWords:
                 return False
         return True
 
-    def sorted_tuples(self, word_score_dict):
+    @staticmethod
+    def sorted_tuples(word_score_dict: Dict[str, int]) -> List[Tuple[str, int]]:
+        """
+        Sort the dictionary to give output in the right order (descending by value then ascending by word)
+        :param word_score_dict: Dictionary of string as key and integers as values where score of each words
+        :return: A list of tuples of words and scores with all the highest words ordered in descending order with the
+        highest scoring first.  If several words have the same score they are ordered alphabetically in the ascending
+        order.
+        """
         return sorted(word_score_dict.items(), key=lambda item: (-item[1], item[0]))
 
-    def build_leaderboard_for_word_list(self):
+    def build_leaderboard_for_word_list(self) -> List[str]:
         """
-        Build a leaderboard of the top scoring MAX_LEADERBOAD_LENGTH words from the complete set of valid words.
+        Build a leaderboard of the top scoring MAX_LEADERBOARD_LENGTH words from the complete set of valid words.
         :return: The list of top words.
         """
         output_dict = {}
@@ -66,7 +77,7 @@ class HighScoringWords:
         sorted_tuples = self.sorted_tuples(output_dict)[:self.MAX_LEADERBOARD_LENGTH]
         return list(zip(*sorted_tuples))[0]
 
-    def build_leaderboard_for_letters(self, starting_letters):
+    def build_leaderboard_for_letters(self, starting_letters: str) -> List[str]:
         """
         Build a leaderboard of the top scoring MAX_LEADERBOARD_LENGTH words that can be built using only the letters contained in the starting_letters String.
         The number of occurrences of a letter in the startingLetters String IS significant. If the starting letters are bulx, the word "bull" is NOT valid.
